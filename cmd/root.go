c0b700f670be2382030253339fc4c5a22d0c66d2
AllanCapistrano <allan.capistrano3@gmail.com>
2023-07-10 10:50:35 -0300

    feat: add CLI select

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,15 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/nexidian/gocliselect"
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -20,7 +19,36 @@ var rootCmd = &cobra.Command{
 following the Conventional Commits  (https://www.conventionalcommits.org/).`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	//Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) { 
+		menu := gocliselect.NewMenu("Commit type?")
+
+		menu.AddItem("feat", "a commit that adds new functionality to Uno.")
+		menu.AddItem("fix", "a commit that fixes a bug in Uno.")
+		menu.AddItem(
+			"chore", 
+			`a catch-all type for any other commits. For 
+			instance, if you're implementing a single feature and it makes sense 
+			to divide the work into multiple commits, you should mark one commit 
+			as feat and the rest as chore.`,
+		)
+		menu.AddItem("refactor", "a commit that refactor a functionality.")
+		menu.AddItem("test", "a commit that adds tests.")
+		menu.AddItem(
+			"docs", 
+			"a commit that adds or improves Uno's documentation.",
+		)
+		menu.AddItem("style", "cyan")
+		menu.AddItem("build", "cyan")
+		menu.AddItem("ci", "cyan")
+		menu.AddItem(
+			"perf", 
+			"a commit that improves performance, without functional changes.",
+		)
+
+		choice := menu.Display()
+
+		fmt.Printf("Choice: %s\n", choice)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
--- go.mod
+++ go.mod
@@ -3,10 +3,14 @@ module github.com/allancapistrano/gbc
 go 1.20
 
 require (
+	github.com/nexidian/gocliselect v1.0.0
 	github.com/spf13/cobra v1.7.0
 )
 
 require (
+	github.com/buger/goterm v1.0.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/pkg/term v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20210331175145-43e1dd70ce54 // indirect
 )

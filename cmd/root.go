c83f1d9f965cc46d673e98e04472deb6231e3158
AllanCapistrano <allan.capistrano3@gmail.com>
2023-07-14 10:31:40 -0300

    feat: commit messages with or without emojis

--- cmd/root.go
+++ cmd/root.go
@@ -25,50 +25,85 @@ simple way to write commits following the Conventional Commits
 (https://www.conventionalcommits.org/).`,
 	Run: func(cmd *cobra.Command, args []string) {
 		commitTypeMenu := gocliselect.NewMenu("Commit type?")
-		gbcEmoji := config.GetEmojis("config/gbc.conf")
+		gbcEmojis := config.GetEmojis("config/gbc.conf")
+		enableEmojis := config.EnableEmojis("config/gbc.conf")
 
 		commitTypeMenu.AddItem(
-			emoji.Sprintf("%sFeature", gbcEmoji.Feat),
+			emoji.Sprintf("%sFeature", gbcEmojis.Feat),
 			"feat",
 		)
 		commitTypeMenu.AddItem(
-			emoji.Sprintf("%sBug Fix", gbcEmoji.Fix),
+			emoji.Sprintf("%sBug Fix", gbcEmojis.Fix),
 			"fix",
 		)
 		commitTypeMenu.AddItem(
-			emoji.Sprintf("%sChore", gbcEmoji.Chore),
+			emoji.Sprintf("%sChore", gbcEmojis.Chore),
 			"chore",
 		)
 		commitTypeMenu.AddItem(
-			emoji.Sprintf("%sRefactor", gbcEmoji.Refactor),
+			emoji.Sprintf("%sRefactor", gbcEmojis.Refactor),
 			"refactor",
 		)
 		commitTypeMenu.AddItem(
-			emoji.Sprintf("%sTests", gbcEmoji.Test),
+			emoji.Sprintf("%sTests", gbcEmojis.Test),
 			"test",
 		)
 		commitTypeMenu.AddItem(
-			emoji.Sprintf("%sDocumentation", gbcEmoji.Docs),
+			emoji.Sprintf("%sDocumentation", gbcEmojis.Docs),
 			"docs",
 		)
 		commitTypeMenu.AddItem(
-			emoji.Sprintf("%sStyle/Clean Up", gbcEmoji.Style),
+			emoji.Sprintf("%sStyle/Clean Up", gbcEmojis.Style),
 			"style",
 		)
 		commitTypeMenu.AddItem(
-			emoji.Sprintf("%sBuild", gbcEmoji.Build),
+			emoji.Sprintf("%sBuild", gbcEmojis.Build),
 			"build",
 		)
 		commitTypeMenu.AddItem(
-			emoji.Sprintf("%sCI", gbcEmoji.Ci),
+			emoji.Sprintf("%sCI", gbcEmojis.Ci),
 			"ci",
 		)
 		commitTypeMenu.AddItem(
-			emoji.Sprintf("%sPerformance Improvement", gbcEmoji.Perf),
+			emoji.Sprintf("%sPerformance Improvement", gbcEmojis.Perf),
 			"perf",
 		)
 
 		commitType := commitTypeMenu.Display()
+
+		if enableEmojis {
+			var commitTypeEmoji string
+
+			switch commitType {
+			case "feat":
+				commitTypeEmoji = gbcEmojis.Feat
+			case "fix":
+				commitTypeEmoji = gbcEmojis.Fix
+			case "chore":
+				commitTypeEmoji = gbcEmojis.Chore
+			case "refactor":
+				commitTypeEmoji = gbcEmojis.Refactor
+			case "test":
+				commitTypeEmoji = gbcEmojis.Test
+			case "docs":
+				commitTypeEmoji = gbcEmojis.Docs
+			case "style":
+				commitTypeEmoji = gbcEmojis.Style
+			case "build":
+				commitTypeEmoji = gbcEmojis.Build
+			case "ci":
+				commitTypeEmoji = gbcEmojis.Ci
+			case "perf":
+				commitTypeEmoji = gbcEmojis.Perf
+			}
+
+			commitType = emoji.Sprintf(
+				"%s%s",
+				commitTypeEmoji,
+				commitType,
+			)
+		}
+
 		buffer := bufio.NewReader(os.Stdin)
 
 		fmt.Print("What commit message do you want? ")

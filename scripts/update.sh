71ec194aa0e2b8b4699418ef80c431b8a97441f0
AllanCapistrano <allan.capistrano3@gmail.com>
2023-07-27 09:28:23 -0300

    feat: create a script to update gbc to the latest version

---
+++ scripts/update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Removing older version.
+sudo rm /usr/bin/gbc
+
+# Downloading the latest version.
+curl -LO https://github.com/AllanCapistrano/gbc/releases/download/v1.1.1/gbc
+
+# Moving the CLI and settings file.
+sudo mv gbc /usr/bin
+
+# Giving permission to run the CLI.
+sudo chmod +x /usr/bin/gbc
+
+gbc --help
+
+echo "gbc updated to version 1.1.1"

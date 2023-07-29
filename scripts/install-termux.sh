8756b7f695f179a39dd448a8820cb40097f9606c
AllanCapistrano <allan.capistrano3@gmail.com>
2023-07-29 16:00:24 -0300

    fix: gbc version

--- scripts/install-termux.sh
+++ scripts/install-termux.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Downloading the necessary files.
-curl -LO https://github.com/AllanCapistrano/gbc/releases/download/v1.2.0/gbc
+curl -LO https://github.com/AllanCapistrano/gbc/releases/download/v1.1.2/gbc
 curl -O https://raw.githubusercontent.com/AllanCapistrano/gbc/main/config/gbc.conf
 
 # Creating the config directory for gbc.
 mkdir -p $HOME/.config/gbc
 
 # Moving the CLI and settings file.
-sudo mv gbc /usr/bin
+mv gbc $PATH
 mv gbc.conf $HOME/.config/gbc
 
 # Giving permission to run the CLI.
-sudo chmod +x /usr/bin/gbc
+chmod +x $PATH/gbc
 
 gbc --help
